f790122f9b6518a8dacfbab8a6008724201d0617
SalmonTT <simontienz@gmail.com>
2020-10-22 18:25:41 +0800

    implemented decision tree classifier with optimized parameters. test result scored an accuracy of 0.851

--- task2.py
+++ task2.py
@@ -2,6 +2,11 @@ import sklearn
 import pandas as pd
 import matplotlib.pyplot as plt
 import category_encoders as ce
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
 
 def printFullDf(df):
     # prints the dataframe in full
@@ -13,13 +18,9 @@ def printFullRow(df):
     pd.set_option('display.max_columns', None)
     print(df)
 
-
 def corrTest(df):
     # Part 1 -----------
     # Test the correlation between attributes/features (attributes and features will be used interchangeably):
-    # 'CreditScore' 'Geography' 'Gender' 'Age' 'Tenure' 'Balance' 'NumOfProducts' 'HasCrCard' 'IsActiveMember'
-    # 'EstimatedSalary'
-
     # drop the following column from dataframe
     feature = df.drop(['CustomerId', 'Exited'], axis=1)
     feature_corr = feature.corr(method='pearson')
@@ -28,7 +29,7 @@ def corrTest(df):
     # show the correlation heatmap
     plt.matshow(feature_corr)
     plt.show()
-    # from the corr matrix we see that most features are uncorrelated,
+    # from the corr matrix we see that most features are uncorrelated (excluding geography),
     # with the exception of 'Balance' and 'NumOfProducts' --> Pearson value = -0.308252
 
     # Part 2 -----------
@@ -38,8 +39,7 @@ def corrTest(df):
     output_corr = abs(feature_output_corr['Exited'])
     printFullDf(output_corr)
     # 'Age' show significant correlation to output 'Exited' --> Pearson value = 0.289895
-    # 'IsActiveMember' is moderately correlated to 'Exited' --> Pearson value = 0.159906
-    # 'Balance' is moderately correlated to 'Exited' --> Pearson value = 0.127389
+    # 'IsActiveMember', 'Gender', 'Balance', 'Geography' are all moderately related
     return
 
 def oneHotEncoding(df):
@@ -51,26 +51,84 @@ def binaryEncoding(df):
     encoder = ce.BinaryEncoder(cols=['Geography'])
     df_binary = encoder.fit_transform(df)
     return df_binary
+
+def print_score(clf, X_train, y_train, X_test, y_test, train=True):
+    if train:
+        pred = clf.predict(X_train)
+        print("Train Result:\n===========================================")
+        print(f"accuracy score: {accuracy_score(y_train, pred):.4f}\n")
+        print(
+            f"Classification Report: \n \tPrecision: {precision_score(y_train, pred)}\n\tRecall Score: {recall_score(y_train, pred)}\n\tF1 score: {f1_score(y_train, pred)}\n")
+        print(f"Confusion Matrix: \n {confusion_matrix(y_train, clf.predict(X_train))}\n")
+
+    elif train == False:
+        pred = clf.predict(X_test)
+        print("Test Result:\n===========================================")
+        print(f"accuracy score: {accuracy_score(y_test, pred)}\n")
+        print(
+            f"Classification Report: \n \tPrecision: {precision_score(y_test, pred)}\n\tRecall Score: {recall_score(y_test, pred)}\n\tF1 score: {f1_score(y_test, pred)}\n")
+        print(f"Confusion Matrix: \n {confusion_matrix(y_test, pred)}\n")
+
+def decisionTree(X_train, X_test, y_train, y_test):
+    # Tuning the hyper-parameters
+    params = {
+        "criterion": ("gini", "entropy"),
+        "splitter": ("best", "random"),
+        "max_depth": (list(range(1, 20))),
+        "min_samples_split": [2, 3, 4],
+        "min_samples_leaf": list(range(1, 20)),
+    }
+
+    model = DecisionTreeClassifier(random_state=42)
+    # n_jobs = -1: using all processors, cv=5: 5 cross-validation
+    grid_search_cv = GridSearchCV(model, params, scoring="accuracy", verbose=1, n_jobs=-1, cv=5)
+    grid_search_cv.fit(X_train, y_train)
+    print(grid_search_cv.best_params_)
+    # grid.search_cv.best_estimator_ is a DecisionTreeClassifier(max_depth=6, random_state=42, splitter='random')
+    print_score(grid_search_cv.best_estimator_, X_train, y_train, X_test, y_test, train=True)
+    print_score(grid_search_cv.best_estimator_, X_train, y_train, X_test, y_test, train=False)
+    # train result: 0.8516, test result: 0.8511111111111112
+    return
+
 def main():
     # ----- loading data -----
-    # df_test: 7500*13
+    # train: 7500*13
     train = pd.read_csv('train.csv', header=0)
     # drop the RowNumber and Surname as they are not important, we keep 'CustomerId' for identification later
     train.drop(['RowNumber', 'Surname'], axis=1, inplace=True)
+
+    # -----Visualizing the distribution of the data for every feature -----
+    train.hist(edgecolor='black', linewidth=1.2, figsize=(20, 20))
+    # --- Observations ---:
+    # 'CreditScore' follows a Gausian distribution (relatively)
+    # 'Age' follows Gausian distribution skewed to the left
+    # 'Balance' has extreme outliers
+    # most people have less than 3 products
+    # 'EstimateSalary' has low variance, may not be very informative
+    # 'IsActiveMember' has high entropy: about half the people are active
+    # 'HasCrCard' has relatively low entropy: about 2/3 have credit card
 
     # ----- feature engineering -----
     # change 'Gender' into numeric value
     train['Gender'].replace({'Male': 0, 'Female': 1}, inplace=True)
     # using the onehot method to label 'Geography'
     train_onehot = oneHotEncoding(train)
-    print(train_onehot)
+    # print(train_onehot)
     # using the binaryEncoding method to label 'Geography'
     train_binary_encoding = binaryEncoding(train)
-    printFullRow(train_binary_encoding)
+    # printFullRow(train_binary_encoding)
 
+    # ----- correlation test (Pearson) -----
+    corrTest(train_onehot)
 
-    # corrTest(train)
-
+    # ----- split training data -----
+    # will consider k-fold cross-validation later
+    X = train_onehot.drop(['Exited'], axis=1)
+    y = train.Exited
+    # test_size is the percentage of data allocated to test
+    # random_state is a seed for random sampling
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    decisionTree(X_train, X_test, y_train, y_test)
 
 if __name__ == "__main__":
     main()
