13326b28e9b53c3d49a97ddde65869206d480b4e
piperzeng <hongni7@126.com>
2020-11-09 21:00:48 +0800

    Add one tune function for random forest

--- randomForest.py
+++ randomForest.py
@@ -23,6 +23,25 @@ def randomForest(X_train, X_test, y_train, y_test):
     ROC(rf, X_train, y_train, X_test, y_test, train=True)
     ROC(rf, X_train, y_train, X_test, y_test, train=False)
 
+def TuneRandomForest(X_train, X_test, y_train, y_test):
+    max_depth = [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    RFtrainAcc = []
+    RFtestAcc = []
+    for param in max_depth:
+        rf = RandomForestClassifier(max_depth=param , min_samples_leaf=4, n_estimators=140, random_state=42)
+        rf.fit(X_train, y_train)
+        y_predTrain = rf.predict(X_train)
+        y_predTest = rf.predict(X_test)
+        RFtrainAcc.append(accuracy_score(y_train, y_predTrain))
+        RFtestAcc.append(accuracy_score(y_test, y_predTest))
+
+    plt.plot(max_depth, RFtrainAcc, 'ro-', max_depth, RFtestAcc, 'bv--')
+    plt.legend(['Training Accuracy', 'Test Accuracy'])
+    plt.xlabel('max_depth')
+    plt.xscale('log')
+    plt.ylabel('Accuracy')
+    plt.show()
+
 
 def useRandomForest():
     train = getTrainingData('train.csv', visualize=False)
@@ -30,6 +49,7 @@ def useRandomForest():
     standard(X)
     y = train.Exited
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-    randomForest(X_train, X_test, y_train, y_test)
+    # randomForest(X_train, X_test, y_train, y_test)
+    TuneRandomForest(X_train, X_test, y_train, y_test)
 
 useRandomForest()
